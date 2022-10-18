76c199f073f3997d63bffd41ade4fc70ad831068
“seoyyy” <seoyyy@gmail.com>
2022-10-18 15:20:49 +0900

    add comment App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './TodoForm';
+import TodoApp_Boyaa from './TodoApp_seoyyy';
 
 function App() {
   return (
     <div className="App">
-     
+     <TodoApp_seoyyy />
      Learn React
      
     </div>
   );
 }
+// App components Seoyoung han
 
 export default App;
